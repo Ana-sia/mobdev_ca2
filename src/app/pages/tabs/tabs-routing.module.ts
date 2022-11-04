7fd326c81f637012be3a138cf43d6e98388d13e3
Ana-sia <2022067@student.cct.ie>
2022-11-04 15:27:35 +0000

    changed pages/tabs/tabs-routing.module.ts

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -21,12 +21,50 @@ const routes: Routes = [
           }
         ]
       },
+
+      {
+        path: 'spaceships',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../spaceships/spaceships.module').then( m => m.SpaceshipsPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../spaceship-details/spaceship-details.module').then( m => m.SpaceshipDetailsPageModule)
+          }
+        ]
+      },
+
+      {
+        path: 'species',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../species/species.module').then( m => m.SpeciesPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../specimen-details/specimen-details.module').then( m => m.SpecimenDetailsPageModule)
+          }
+        ]
+      },
       
     ]
   },
   {
     path: '',
     redirectTo: '/tabs/films',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/spaceships',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/species',
     pathMatch: 'full'
   }
 ];
