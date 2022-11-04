9404ebd2d331ceb3228438e762997fc81c5a9e33
Ana-sia <2022067@student.cct.ie>
2022-11-04 15:15:07 +0000

    Created new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,27 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
+  {
+    path: 'species',
+    loadChildren: () => import('./pages/species/species.module').then( m => m.SpeciesPageModule)
+  },
+  {
+    path: 'speciesspaceships',
+    loadChildren: () => import('./pages/speciesspaceships/speciesspaceships.module').then( m => m.SpeciesspaceshipsPageModule)
+  },
+  {
+    path: 'specimen-details',
+    loadChildren: () => import('./pages/specimen-details/specimen-details.module').then( m => m.SpecimenDetailsPageModule)
+  },
+  {
+    path: 'spaceship-details',
+    loadChildren: () => import('./pages/spaceship-details/spaceship-details.module').then( m => m.SpaceshipDetailsPageModule)
+  },
+  {
+    path: 'spaceships',
+    loadChildren: () => import('./pages/spaceships/spaceships.module').then( m => m.SpaceshipsPageModule)
+  }
 ];
 
 @NgModule({
